c5e0df224e7114029e165a20d633e1b52b0c53b1
Dovydas Rimkus <drdovy@hotmail.com>
2022-08-05 11:04:01 +0100

    added a test and renamed test methods

--- src/test/java/com/sparta/dr/PalindromeTest.java
+++ src/test/java/com/sparta/dr/PalindromeTest.java
@@ -5,27 +5,36 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeTest {
+
     @Test
-    @DisplayName("Given the input has capital letters, return palindrome correctly")
-    void capitalLetters() {
+    @DisplayName("Given the input is a palindrome return true")
+    void givenTheInputIsAPalindromeReturnTrue() {
         Boolean expected = true;
-        Boolean answer = MyStrings.palindrome("rAcEcar");
+        Boolean answer = MyStrings.palindrome("hannah");
+        Assertions.assertEquals(expected, answer);
+    }
+
+    @Test
+    @DisplayName("Given the input has capital letters, return true")
+    void givenTheInputHasCapitalLettersReturnTrue() {
+        Boolean expected = true;
+        Boolean answer = MyStrings.palindrome("rAcEcAr");
         Assertions.assertEquals(expected, answer);
     }
     @Test
     @DisplayName("Given the input is not a palindrome, return false")
-    void notPalindrome() {
+    void givenTheInputIsNotAPalindromeReturnFalse() {
         Boolean expected = false;
         Boolean answer = MyStrings.palindrome("Test");
         Assertions.assertEquals(expected, answer);
     }
     @Test
     @DisplayName("Given the input is an empty string, return false")
-    void nullValue() {
+    void givenTheInputIsAnEmptyStringReturnFalse() {
         Boolean expected = false;
         Boolean answer = MyStrings.palindrome("");
         Assertions.assertEquals(expected, answer);
-
 }
+
 
 }
